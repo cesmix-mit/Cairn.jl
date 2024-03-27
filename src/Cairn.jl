b634edd4d62ce092a7f77f12dd2ce087c66a2cf8
joannajzou <joannajzou@gmail.com>
2024-03-27 11:11:09 -0400

    add dependencies
    Add external package dependencies to Cairn.jl and Project.toml.

--- src/Cairn.jl
+++ src/Cairn.jl
@@ -1,5 +1,34 @@
+"""
+
+Cairn.jl
+
+Author: cesmix-mit
+Version: 0.1.0
+Year: 2024
+Notes: Enhanced sampling algorithms for the active learning of machine learning interatomic potentials (ML-IPs), implemented in Julia.
+"""
+
 module Cairn
 
-# Write your package code here.
+using LinearAlgebra 
+using Statistics
+using StatsBase
+using StaticArrays
+using Random
+using Distributions
+using Polynomials
+using SpecialPolynomials
+using AtomsBase
+using Unitful
+using UnitfulAtomic
+using Molly
+using InteratomicPotentials
+using PotentialLearning
+using ActiveSubspaces
+using Maxvol
+
+
+
+
 
 end
