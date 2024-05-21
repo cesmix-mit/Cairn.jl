1df494675881505a682754e82f89f0a48f847468
joannajzou <joannajzou@gmail.com>
2024-05-21 17:08:08 -0400

    add observable value to TriggerLogger
    record value of trigger.eval at current time step

--- src/loggers/triggerlogger.jl
+++ src/loggers/triggerlogger.jl
@@ -8,35 +8,42 @@ export TriggerLogger
 A logger which holds a record of evaluations of the trigger function for active learning. 
 
 # Arguments
-- `trigger::ActiveLearningTrigger`   : trigger function.
-- `n_steps::Int`   : time step interval at which the trigger function is evaluated.
-- `history::Vector{T}`   : record of the trigger function evaluation.
+- `trigger::ActiveLearningTrigger`      : trigger function.
+- `observable::T`                       : value of the trigger function of type `T`. 
+- `n_steps::Int`                        : time step interval at which the trigger function is evaluated.
+- `history::Vector{T}`                  : record of the trigger function evaluation.
 """
 mutable struct TriggerLogger{A, T}
     trigger::A
+    observable::T
     n_steps::Int
     history::Vector{T}
 end
 
 
 function TriggerLogger(trigger::ActiveLearningTrigger, T::DataType, n_steps::Integer)
-    return TriggerLogger{typeof(trigger), T}(trigger, n_steps, T[])
+    return TriggerLogger{typeof(trigger), T}(trigger, T[], n_steps, T[])
 end
 TriggerLogger(trigger::ActiveLearningTrigger, n_steps::Integer) = TriggerLogger(trigger, Float64, n_steps)
 
 
+Base.values(logger::TriggerLogger) = logger.history
+
+
 function log_property!(logger::TriggerLogger, s::System, neighbors=nothing,
     step_n::Integer=0; n_threads::Integer=Threads.nthreads(), kwargs...)
+    
+    obs = logger.trigger.eval(s)
+    logger.observable = obs
+
     if (step_n % logger.n_steps) == 0
-        if typeof(logger.trigger) <: Union{UpperThreshold, LowerThreshold, MaxVol}
-            obs = logger.trigger.eval(; kwargs...)
+        if typeof(logger.trigger) <: Union{Bool, TimeInterval} 
+            return
+        else
             push!(logger.history, obs)
         end
     end
 end
-
-
-Base.values(logger::TriggerLogger) = logger.history
 
 
 function Base.show(io::IO, fl::TriggerLogger)
