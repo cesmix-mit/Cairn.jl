685233e962b5262e5c2939bcdaba56dd976ef5a8
joannajzou <joannajzou@gmail.com>
2024-05-23 13:26:18 -0400

    update examples

---
+++ examples/mullerbrown_train.jl
@@ -0,0 +1,211 @@
+using Cairn
+using LinearAlgebra, Random, Statistics, StatsBase, Distributions
+using PotentialLearning
+using Molly, AtomsCalculators
+using AtomisticQoIs
+using SpecialPolynomials, SpecialFunctions
+
+include("./src/makie/makie.jl")
+include("./examples/utils.jl")
+
+
+
+## define models ------------------------------------------------------------------
+# choose reference model
+ref = MullerBrownRot()
+
+# define main support
+limits = [[-4.4,1.5],[-2,2]]
+# limits = [[-3.5,1.5],[-1.5,3.5]]
+coord_grid = coord_grid_2d(limits, 0.05)
+ctr_lvls = -150:50:1000
+
+# PCE properties
+basisfam = Jacobi{0.5,0.5}
+order = 20
+pce0 = PolynomialChaos(order, 2, basisfam, xscl=limits)
+
+# grid over main support
+coords_eval = potential_grid_2d(ref, limits, 0.04, cutoff = 800)
+sys_eval = define_ens(ref, coords_eval)
+
+# use grid to define uniform quadrature points
+ξ = [ustrip.(Vector(coords)) for coords in coords_eval]
+GQint = GaussQuadrature(ξ, ones(length(ξ))./length(ξ))
+
+# plot
+f0, ax0 = plot_contours_2d(ref, coord_grid; fill=true, lvls=ctr_lvls2)
+coordmat = reduce(hcat, get_values(coords_eval))'
+scatter!(ax0, coordmat[:,1], coordmat[:,2], color=:red, markersize=5, label="test points")
+axislegend(ax0)
+f0
+
+# plot density 
+f, _ = plot_density(ref, coord_grid, GQint)
+
+
+# reference: train to test set
+pce = deepcopy(pce0)
+lp = learn!(sys_eval, ref, pce, [1000,1], false; e_flag=true, f_flag=true)
+p = define_gibbs_dist(ref)
+q = define_gibbs_dist(pce, θ=lp.β)
+fish = FisherDivergence(GQint)
+fd_best = compute_divergence(p, q, fish)
+
+
+## training set 1: grid over main support ---------------------------------------
+# sample from grid
+coords1 = potential_grid_2d(ref, limits, 0.05, cutoff = 800)
+trainset1 = define_ens(deepcopy(pce0), coords1)
+
+# plot
+f0, ax0 = plot_contours_2d(ref, coord_grid; fill=true, lvls=ctr_lvls)
+coordmat = reduce(hcat, get_values(coords1))'
+scatter!(ax0, coordmat[:,1], coordmat[:,2], color=:red, markersize=5, label="train set 1")
+axislegend(ax0)
+f0
+
+
+
+## training set 2: samples from Langevin MD -------------------------------------
+# Langevin simulator
+sim_langevin = OverdampedLangevin(
+            dt=0.002u"ps",
+            temperature=500.0u"K",
+            friction=4.0u"ps^-1",
+)
+sys0 = define_sys(
+            ref,
+            [0.5, 0.5],
+            loggers=(coords=CoordinateLogger(100; dims=2),),
+)
+# simulate
+sys2 = deepcopy(sys0)
+simulate!(sys2, sim_langevin, 1_000_000)
+
+# subselect train data from the trajectory
+id = StatsBase.sample(1:length(sys2.loggers.coords.history), length(coords1), replace=false)
+coords2 = [sys2.loggers.coords.history[i][1] for i in id] 
+trainset2 = define_ens(deepcopy(pce0), coords2)
+
+# plot
+f, ax = plot_contours_2d(ref, coord_grid; fill=true, lvls=ctr_lvls)
+coordmat = reduce(hcat, get_values(coords2))'
+scatter!(ax, coordmat[:,1], coordmat[:,2], color=:red, markersize=5, label="train set 2")
+axislegend(ax)
+f
+
+
+
+## training set 3: samples from high-T MD -------------------------------------
+# high-temp Langevin simulator
+sim_highT = OverdampedLangevin(
+            dt=0.002u"ps",
+            temperature=1500.0u"K",
+            friction=4.0u"ps^-1",
+)
+# simulate
+sys3 = deepcopy(sys0)
+simulate!(sys3, sim_highT, 1_000_000)
+# f = plot_md_trajectory(sys3, coord_grid, fill=false, lvls=ctr_lvls, showpath=false)
+
+# subselect train data from the trajectory
+id = StatsBase.sample(1:length(sys3.loggers.coords.history), length(coords1), replace=false)
+coords3 = [sys3.loggers.coords.history[i][1] for i in id] 
+trainset3 = define_ens(deepcopy(pce0), coords3)
+
+# plot
+f, ax = plot_contours_2d(ref, coord_grid; fill=true, lvls=ctr_lvls)
+coordmat = reduce(hcat, get_values(coords3))'
+scatter!(ax, coordmat[:,1], coordmat[:,2], color=:red, markersize=5, label="train set 3")
+axislegend(ax)
+f
+
+
+# train with changing weight λ --------------------------------------------------------------
+λarr = [1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3, 1e4]
+trainsets = [trainset1, trainset2, trainset3]
+p = define_gibbs_dist(ref)
+fish = FisherDivergence(GQint)
+
+
+# store results
+param_dict = Dict( "ts$j" => Dict(
+    "E" => zeros(length(pce.basis)),
+    "F" => zeros(length(pce.basis)), 
+    "EF" => Vector{Vector}(undef, length(λarr)),
+    ) for j = 1:length(trainsets)
+)
+
+err_dict = Dict( "ts$j" => Dict(
+    "E" => 0.0,
+    "F" => 0.0, 
+    "EF" => zeros(length(λarr)),
+    ) for j = 1:length(trainsets)
+)
+
+fd_dict = Dict( "ts$j" => Dict(
+    "E" => 0.0,
+    "F" => 0.0, 
+    "EF" => zeros(length(λarr)),
+    ) for j = 1:length(trainsets)
+)
+
+
+# train on E or F only (UnivariateLinearProblem)
+for (j,ts) in enumerate(trainsets)
+    # E objective
+    println("train set $j, E only")
+    pce = deepcopy(pce0)
+    lpe = learn!(ts, ref, pce; e_flag=true, f_flag=false)
+    q = define_gibbs_dist(pce, θ=lpe.β)
+    err_dict = 
+    fd_dict["ts$j"]["E"] = compute_divergence(p, q, fish)
+    param_dict["ts$j"]["E"] = lpe.β
+
+    # F objective
+    println("train set $j, F only")
+    pce = deepcopy(pce0)
+    lpf = learn!(ts, ref, pce; e_flag=false, f_flag=true)
+    q = define_gibbs_dist(pce, θ=lpf.β)
+    fd_dict["ts$j"]["F"] = compute_divergence(p, q, fish)
+    param_dict["ts$j"]["F"] = lpf.β
+end
+
+# train on EF (CovariateLinearProblem)
+for (i,λ) in enumerate(λarr)
+    for (j,ts) in enumerate(trainsets)
+        
+        # EF objective
+        println("train set $j, EF (λ=$λ)")
+        pce = deepcopy(pce0)
+        lpef = learn!(ts, ref, pce, [λ, 1], false; e_flag=true, f_flag=true)
+        q = define_gibbs_dist(pce, θ=lpef.β)
+        fd_dict["ts$j"]["EF"][i] = compute_divergence(p, q, fish)
+        param_dict["ts$j"]["EF"][i] = lpef.β
+    end
+end
+
+
+
+# plot results
+λlab = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3, 1e4, 1e5]
+f = Figure(resolution=(550,450))
+ax = Axis(f[1,1],
+    xlabel="λ",
+    ylabel="Fisher divergence",
+    title="Model Error vs. Weight λ",
+    xscale=log10,
+    yscale=log10,
+    xticks=(λlab, ["F", "1e-4", "1e-3", "1e-2", "1e-1", "1", "1e1", "1e2", "1e3", "1e4", "E"]))
+
+for j = 1:3
+    fd_all = reduce(vcat, [[fd_dict["ts$j"]["F"]], fd_dict["ts$j"]["EF"], [fd_dict["ts$j"]["E"]]])
+    scatterlines!(ax, λlab, fd_all, label="train set $j")
+end
+axislegend(ax, position=:lb)
+f
+
+pce.params = param_dict["ts2"]["EF"][9]
+ctr_lvls2 = -100:25:200
+f, _ = plot_contours_2d(pce, coord_grid, fill=true, lvls=ctr_lvls2)
