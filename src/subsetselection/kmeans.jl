ea8e963965a122af861b0373c8176742ad676bcb
joannajzou <joannajzou@gmail.com>
2025-01-31 15:36:41 -0500

    update kmeans.jl

--- src/subsetselection/kmeans.jl
+++ src/subsetselection/kmeans.jl
@@ -2,13 +2,13 @@ import PotentialLearning: get_random_subset
 export KMeans
 
 struct KMeans <: SubsetSelector
-    assign :: Vector
-    c :: Vector
+    assign :: Vector{<:Int}
+    c :: Vector{<:Int}
     batch_size :: Int
 end
-function KMeans(assign::Vector, c::Vector, batch_size::Int)
-    return KMeans(assign, c, batch_size)
-end
+# function KMeans(assign::Vector, c::Vector, batch_size::Int)
+#     return KMeans(assign, c, batch_size)
+# end
 function KMeans(desc::Vector, k::Int, batch_size::Int)
     A = Matrix(reduce(hcat, desc))
     res = kmeans(A, k)
