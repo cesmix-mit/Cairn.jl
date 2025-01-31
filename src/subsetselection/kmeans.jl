a11c845e9cb2a78a32553158190bb379353ac396
joannajzou <joannajzou@gmail.com>
2025-01-31 15:04:28 -0500

    update KMeans struct

--- src/subsetselection/kmeans.jl
+++ src/subsetselection/kmeans.jl
@@ -2,26 +2,31 @@ import PotentialLearning: get_random_subset
 export KMeans
 
 struct KMeans <: SubsetSelector
-    desc :: Vector
-    k :: Int
+    assign :: Vector
+    c :: Vector
     batch_size :: Int
 end
-
+function KMeans(assign::Vector, c::Vector, batch_size::Int)
+    return KMeans(assign, c, batch_size)
+end
+function KMeans(desc::Vector, k::Int, batch_size::Int)
+    A = Matrix(reduce(hcat, desc))
+    res = kmeans(A, k)
+    return KMeans(assignments(res), counts(res), batch_size)
+end
+function KMeans(dist::Union{Symmetric{T, Matrix{T}}, Matrix{T}}, k::Int, batch_size::Int) where {T}
+    res = kmedoids(dist, k)
+    return KMeans(assignments(res), counts(res), batch_size)
+end
 
 function get_random_subset(
     km::KMeans,
 )
-    A = Matrix(reduce(hcat, km.desc))
-    res = kmeans(A, km.k)
-
-    a = assignments(res) # get the assignments of points to clusters
-    c = counts(res) # get the cluster sizes
-
-    prop = c ./ length(km.desc)
+    prop = km.c ./ length(km.assign)
     nsamp = Int.(ceil.(prop .* km.batch_size))
     indices = Int64[]
     for (i,n) in enumerate(nsamp)
-        id = StatsBase.sample(findall(x -> x == i, a), n, replace=false)
+        id = StatsBase.sample(findall(x -> x == i, km.assign), n, replace=false)
         append!(indices, id)
     end
     return StatsBase.sample(indices, km.batch_size, replace=false)
